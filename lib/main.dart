89b12e7e03f8791f5f7d546487e1a4c44ea811a9
FlutLab.io <>
2021-03-02 00:00:21 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favourite color?',
+        'answers': [
+          'Black',
+          'Red',
+          'Green',
+          'White'
+        ],
+      },
+      {
+        'questionText': "What's your favourite animal?",
+        'answers': [
+          'Rabbit',
+          'Snake',
+          'Elephant',
+          'Lion'
+        ],
+      },
+      {
+        'questionText': "What's your favourite language?",
+        'answers': [
+          'C',
+          'Kotlin',
+          'Flutter',
+          'Java'
+        ],
+      }
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("My First App")),
+          body: Column(
+            children: [
+              Question(questions[_questionIndex]['questionText']), //Text Question on top
+              ...(questions[_questionIndex]['answers'] as List<String>).map((answer) { // ... insert all the values of a list into another list
+                return Answer(_answerQuestion, answer); // map transform that list into widget & generate new list based on old list
+              }).toList() //convert the value map give us to a list
+            ],
+          )),
+    );
+  }
+}
